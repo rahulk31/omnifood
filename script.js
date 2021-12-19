db107472ec110d844f5a94639483ab6f054eccdd
Rahul <rahulsingh3118.rs@gmail.com>
2021-12-19 13:49:01 +0530

    fixed mobile menu bugs and implemented smooth scroll behaviour

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const header = document.querySelector('.header');
 const menu = document.querySelector('.icon-mobile-nav[name="grid-outline"]');
 const close = document.querySelector('.icon-mobile-nav[name="close-outline"]');
+const year = document.querySelector('.year');
+const links = document.querySelectorAll('.main-nav-link');
+
+const currDate = new Date().getFullYear();
+year.textContent = currDate
 
 
 const openMenu = () => {
@@ -14,6 +19,16 @@ const closeMenu = () => {
     close.style.display = 'none';
 }
 
+const clickHandler = () => {
+    header.classList.remove('nav-open');
+    menu.style.display = 'block';
+    close.style.display = 'none';
+}
+
 
 menu.addEventListener('click', openMenu);
 close.addEventListener('click', closeMenu);
+links.forEach(link => {
+    console.log(link);
+    link.addEventListener('click', clickHandler);
+});
